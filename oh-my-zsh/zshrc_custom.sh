7873dc7264864ed820093185452427c29dfef02c
Alex Cummaudo <alexcu@me.com>
2020-10-10 09:28:51 +1100

    Make sure emacs is always default editor

--- oh-my-zsh/zshrc_custom.sh
+++ oh-my-zsh/zshrc_custom.sh
@@ -1,72 +1,22 @@
-# Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='nano'
-else
-  export EDITOR='emacs'
-fi
+# Always use emacs
+export EDITOR='emacs'
+export VISUAL='emacs'
 
 # Prefer /usr/local/bin over /usr/bin for brew installs
 export PATH=/usr/local/bin:$PATH
 export PATH="$HOME/.jenv/bin:$PATH"
+export PATH="/usr/local/opt/sqlite/bin:$PATH"
 
-#
-# Functions
-#
-
-# Easy VNC
+# Aliases & Functions
 vnc() { open vnc://$1 }
-
-# List my repos
-repos() { cd ~/repos/$1; ls }
-
-# CD to Desktop
-cdd() { cd ~/Desktop }
-
-# Ssh via back to my mac (btmm)
-btmm_account_no()
-{
-  PIDFILE=$(mktemp)
-  ACCNO=$(sed -n '/^[ ->]*\([0-9]\{10\}\)$/{s//\1/p;q;}' <(dns-sd -E & echo $! >> $PIDFILE))
-  kill $(cat $PIDFILE)
-  rm $PIDFILE
-  echo $ACCNO
-}
-sshmac() { ssh Alex@$1.$(btmm_account_no).members.btmm.icloud.com }
-
-# Edit this file
+alias vpn="~/.dotfiles/vpn"
+alias cdd="cd ~/Desktop"
 alias zshconfig="emacs ~/.zshrc"
-
-#
-# Aliases
-#
-
-# Repositories
-alias dfire-api="cd ~/repos/doubtfire/doubtfire-api"
-alias dfire-web="cd ~/repos/doubtfire/doubtfire-web"
-alias phd="cd ~/PhD"
-alias phdtex="cd ~/PhD/LaTeX"
-alias phdexp="cd ~/PhD/Experiments"
-
-
-# Apps
-alias color="osascript '/Users/Alex/Library/Mobile Documents/com~apple~ScriptEditor2/Documents/ColorPicker.scpt'"
-alias texpad="open -a Texpad"
-
-# Service Shortcuts
 alias cpwd="pwd | pbcopy"
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 alias sleepdisplay="pmset displaysleepnow"
 
-#
-# Evals
-#
-
-# User configuration
-eval "$(/usr/libexec/path_helper -s)"
-
 # Initialise rbenv + pyenv + jenv
-eval "$(rbenv init -)"
-eval "$(pyenv init -)"
-
-export PATH="/usr/local/opt/sqlite/bin:$PATH"
-export HOMEBREW_AUTO_UPDATE_SECS=3600
+if [ -x "$(command -v rbenv)" ]; then eval "$(rbenv init -)"; fi
+if [ -x "$(command -v pyenv)" ]; then eval "$(pyenv init -)"; fi
+if [ -x "$(command -v jenv)"  ]; then eval "$(jenv init -)"; fi
